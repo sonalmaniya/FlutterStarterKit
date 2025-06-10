e2466d079b6d7cc54ba02f9a6123aa895c2fd0f3
Sonal <sonal.maniya.94@gmail.com>
2025-06-10 10:01:42 +0530

    fix: flutter localization

---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,14 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get welcome_message =>
+      '¡Bienvenido! Comience su viaje con MyApp. ¡Las posibilidades son infinitas!';
+}
